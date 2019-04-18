555e35dc4dcb37dca29fff0012296759a092b3f7
mathcrypto <Amira.bouguera@consensys.net>
2019-04-18 15:36:41 +0200

    Create GenSig.cpp

---
+++ src/jubjub/GenSig.cpp
@@ -0,0 +1,2 @@
+// In this file, we will generate N signature pairs pub/priv pair, we generate a SNARK (since participants have to send proof that they are eligible) 
+// we will call the file eddsa.cpp each for each player where signature pairs were generated and also a snark for each of those
